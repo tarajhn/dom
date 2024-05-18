7dc03784fd3f92b633a068eddd1e116439874f01
tarajhn <zahrajahangiri6232@gmail.com>
2024-05-18 15:05:53 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function visible(e){
+	const showDiv=document.getElementById('btn-4')
+	Element.classList.toggle('visible')
+
+}
+function showDiv (e){
+	const showDiv=document.getElementById('btn-4')
+	Element.classList.toggle('visible')
+
